d363f66afa5500feff45132a0cd89b0eca67e725
anamontiaga <amontiaga@gmail.com>
2019-10-10 18:01:27 +0200

    add showMessage function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,36 @@
 "use strict";
 
-console.log(">> Ready :)");
+// Nomeo elementos que voy a necesitar
+const number = document.querySelector(".number");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const alert = document.querySelector(".alert");
+
+const handleFunction = ev => {
+  ev.preventDefault();
+  showMessage();
+  console.log(`Mi número aleatorio es ${myRandomNumber}`);
+};
+
+const getRandomNumber = max => {
+  return Math.ceil(Math.random() * max);
+};
+
+const myRandomNumber = getRandomNumber(100);
+
+const write = (domElement, message) => {
+  domElement.innerHTML = message;
+};
+
+const showMessage = () => {
+  let inputValue = parseInt(input.value);
+  if (inputValue === myRandomNumber) {
+    write(alert, "¡HAS GANADO, CAMPEONA!");
+  } else if (inputValue > myRandomNumber) {
+    write(alert, "demasiado alto");
+  } else {
+    write(alert, "demasiado bajo");
+  }
+};
+
+btn.addEventListener("click", handleFunction);
